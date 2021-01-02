3cfe6306188f8b392f150cb29decc6f5ef139dcd
Kohei-jp <kohei.mitateda@gmail.com>
2021-01-02 17:58:41 +0900

    3-9.useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const App = (props) => {
   const [state, setState] = useState(props) //オブジェクトとして持てる
   const {name, price} = state // 分割代入
 
+  // domが描画された後に実行される
+  useEffect(()=> {
+    console.log('This is like componentDidMount or componentDidUpdate');
+  });
+
+  useEffect(()=> {
+    console.log('This is like componentDidMount');
+  },[]);
+
+  useEffect(()=> {
+    console.log('This callback is for name only.');
+  },[name]);
+
+  const renderPeriod = () => {
+    console.log("renderPeriod");
+    return '。'
+  }
   return (
     <>
       <p>現在の{name}は、{price}円です。</p>
